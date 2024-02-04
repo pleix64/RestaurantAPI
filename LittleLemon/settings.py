670d48d057250e52afa017df0a05bdc6ce7e6fd1
Yu Chen <chenyu.bnu@gmail.com>
2024-02-04 02:06:12 -0800

    moved to PostgreSQL

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'little_lemon',
+        'OPTIONS': {
+            'service': 'lemon_service',
+            'passfile': '.pgpass', 
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
