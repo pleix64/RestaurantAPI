47d3a4ba34bf565e68e14af3496d2044dab9fe9d
Yu Chen <chenyu.bnu@gmail.com>
2024-02-12 01:30:21 -0800

    test deploying with docker

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -11,19 +11,28 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jp+fc%+%$#e0c_vt(z0-i!3^fh89*nm)u#g1$@9@*2p@_le9!&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -88,15 +97,26 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': 'localhost',
+#         'NAME': 'little_lemon',
+#         'OPTIONS': {
+#             'service': 'lemon_service',
+#             'passfile': '.pgpass', 
+#         },
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'NAME': 'little_lemon',
-        'OPTIONS': {
-            'service': 'lemon_service',
-            'passfile': '.pgpass', 
-        },
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "HOST": env('DB_HOST'),
+        "PORT": env('DB_PORT'),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
     }
 }
 
