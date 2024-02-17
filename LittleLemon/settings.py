cc0bdbe8cedb52f24b129d1fa14f1cc0b485673f
Yu Chen <chenyu.bnu@gmail.com>
2024-02-17 13:18:18 -0800

    CSRF_TRUSTED_ORIGINS in settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
